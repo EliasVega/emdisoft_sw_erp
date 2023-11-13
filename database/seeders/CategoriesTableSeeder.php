be25c60a28b55e1743e088d772e2554936e78904
EliasVega <discom.is@gmail.com>
2023-11-13 08:17:10 -0500

    modificaciones seeder

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -21,71 +21,146 @@ class CategoriesTableSeeder extends Seeder
             0 =>
             array (
                 'id' => 1,
-                'name' => 'Electricos',
-                'description' => 'Todo lo relacionado con insumos electricos',
+                'name' => 'ALMUERZOS',
+                'description' => 'ALMUERZOS',
                 'utility_rate' => '30.00',
                 'status' => 'active',
-                'company_tax_id' => 2,
-                'created_at' => '2023-01-12 21:07:43',
-                'updated_at' => '2023-01-12 21:07:43',
+                'company_tax_id' => 3,
+                'created_at' =>	'2023-10-20 12:00:00',
+                'updated_at' =>	'2023-10-20 12:00:00'
             ),
             1 =>
             array (
                 'id' => 2,
-                'name' => 'Ferreteria',
-                'description' => 'Todo lo relacionado con Herramientas de Ferreteria',
+                'name' => 'CARNES',
+                'description' => 'CARNES',
                 'utility_rate' => '30.00',
                 'status' => 'active',
-                'company_tax_id' => 2,
-                'created_at' => '2023-01-12 21:07:43',
-                'updated_at' => '2023-01-12 21:07:43',
+                'company_tax_id' => 3,
+                'created_at' =>	'2023-10-20 12:00:00',
+                'updated_at' =>	'2023-10-20 12:00:00'
             ),
             2 =>
             array (
                 'id' => 3,
-                'name' => 'Computadores',
-                'description' => 'Todo lo relacionado con Computacion',
+                'name' => 'DE LA REGION',
+                'description' => 'DE LA REGION',
                 'utility_rate' => '30.00',
                 'status' => 'active',
-                'company_tax_id' => 11,
-                'created_at' => '2023-01-12 21:07:43',
-                'updated_at' => '2023-01-12 21:07:43',
+                'company_tax_id' => 3,
+                'created_at' =>	'2023-10-20 12:00:00',
+                'updated_at' =>	'2023-10-20 12:00:00'
             ),
             3 =>
             array (
                 'id' => 4,
-                'name' => 'tablet',
-                'description' => 'Todo lo relacionado con tablets',
+                'name' => 'DESAYUNOS',
+                'description' => 'DESAYUNOS',
                 'utility_rate' => '30.00',
                 'status' => 'active',
-                'company_tax_id' => 2,
-                'created_at' => '2023-01-12 21:07:43',
-                'updated_at' => '2023-01-12 21:07:43',
+                'company_tax_id' => 3,
+                'created_at' =>	'2023-10-20 12:00:00',
+                'updated_at' =>	'2023-10-20 12:00:00'
             ),
             4 =>
             array (
                 'id' => 5,
-                'name' => 'Productos gravados con INC',
-                'description' => 'Gastos de la empresa',
-                'utility_rate' => '0.00',
+                'name' => 'EMBUTIDOS',
+                'description' => 'EMBUTIDOS',
+                'utility_rate' => '30.00',
                 'status' => 'active',
                 'company_tax_id' => 3,
-                'created_at' => '2023-01-12 21:07:43',
-                'updated_at' => '2023-01-12 21:07:43',
+                'created_at' =>	'2023-10-20 12:00:00',
+                'updated_at' =>	'2023-10-20 12:00:00'
             ),
             5 =>
             array (
                 'id' => 6,
-                'name' => 'Excentos',
-                'description' => 'Gastos de la empresa',
-                'utility_rate' => '0.00',
+                'name' => 'ENTRADAS',
+                'description' => 'ENTRADAS',
+                'utility_rate' => '30.00',
+                'status' => 'active',
+                'company_tax_id' => 3,
+                'created_at' =>	'2023-10-20 12:00:00',
+                'updated_at' =>	'2023-10-20 12:00:00'
+            ),
+            6 =>
+            array (
+                'id' => 7,
+                'name' => 'PASTA',
+                'description' => 'PASTA',
+                'utility_rate' => '30.00',
+                'status' => 'active',
+                'company_tax_id' => 3,
+                'created_at' =>	'2023-10-20 12:00:00',
+                'updated_at' =>	'2023-10-20 12:00:00'
+            ),
+            7 =>
+            array (
+                'id' => 8,
+                'name' => 'PESCADOS',
+                'description' => 'PESCADOS',
+                'utility_rate' => '30.00',
+                'status' => 'active',
+                'company_tax_id' => 3,
+                'created_at' =>	'2023-10-20 12:00:00',
+                'updated_at' =>	'2023-10-20 12:00:00'
+            ),
+            8 =>
+            array (
+                'id' => 9,
+                'name' => 'POLLO',
+                'description' => 'POLLO',
+                'utility_rate' => '30.00',
+                'status' => 'active',
+                'company_tax_id' => 3,
+                'created_at' =>	'2023-10-20 12:00:00',
+                'updated_at' =>	'2023-10-20 12:00:00'
+            ),
+            9 =>
+            array (
+                'id' => 10,
+                'name' => 'PORCIONES',
+                'description' => 'PORCIONES',
+                'utility_rate' => '30.00',
+                'status' => 'active',
+                'company_tax_id' => 3,
+                'created_at' =>	'2023-10-20 12:00:00',
+                'updated_at' =>	'2023-10-20 12:00:00'
+            ),
+            10 =>
+            array (
+                'id' => 11,
+                'name' => 'TIPICOS',
+                'description' => 'TIPICOS',
+                'utility_rate' => '30.00',
+                'status' => 'active',
+                'company_tax_id' => 3,
+                'created_at' =>	'2023-10-20 12:00:00',
+                'updated_at' =>	'2023-10-20 12:00:00'
+            ),
+            11 =>
+            array (
+                'id' => 12,
+                'name' => 'VERDURAS',
+                'description' => 'VERDURAS',
+                'utility_rate' => '30.00',
+                'status' => 'active',
+                'company_tax_id' => 3,
+                'created_at' =>	'2023-10-20 12:00:00',
+                'updated_at' =>	'2023-10-20 12:00:00'
+            ),
+            12 =>
+            array (
+                'id' => 13,
+                'name' => 'PRODUCTO EXCENTO',
+                'description' => 'COMPRA MATERIA PRIMA',
+                'utility_rate' => '13.00',
                 'status' => 'active',
                 'company_tax_id' => 1,
-                'created_at' => '2023-01-12 21:07:43',
-                'updated_at' => '2023-01-12 21:07:43',
-            ),
+                'created_at' =>	'2023-10-20 12:00:00',
+                'updated_at' =>	'2023-10-20 12:00:00'
+            )
         ));
-
-
     }
 }
--- database/seeders/RawMaterialsTableSeeder.php
+++ database/seeders/RawMaterialsTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RawMaterialsTableSeeder extends Seeder
 {
@@ -14,11 +15,9 @@ class RawMaterialsTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('raw_materials')->delete();
 
-
-        \DB::table('raw_materials')->delete();
-
-        \DB::table('raw_materials')->insert(array (
+        DB::table('raw_materials')->insert(array (
             0 =>
             array (
                 'id' => 1,
