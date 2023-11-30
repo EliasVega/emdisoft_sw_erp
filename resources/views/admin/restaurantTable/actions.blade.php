1089987d0516aa2c626a1d21a3effeba5556df07
EliasVega <discom.is@gmail.com>
2023-11-30 11:34:12 -0500

    restriccion de eliminacion mesas a susarios

--- resources/views/admin/restaurantTable/actions.blade.php
+++ resources/views/admin/restaurantTable/actions.blade.php
@@ -2,7 +2,7 @@
     <a href="{{ route('restaurantTable.edit', $id) }}" class="btn btn-warning" data-toggle="tooltip"
     data-placement="top" title="Editar"><i class="far fa-edit"></i></a>
 @endcan
-@can('restaurantTable.destroy')
+@can('superAdmin')
     <a class="btn btn-danger" href="" data-target="#modal-delete-{{ $id }}" data-toggle="modal" title="Eliminar">
         <i class="fas fa-trash fa-fw"></i></a>
 @endcan
