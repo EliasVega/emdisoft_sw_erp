171a590ad5ddf67434413aec42810c470318fa5a
EliasVega <discom.is@gmail.com>
2024-01-14 21:14:34 -0500

    users delimarket

--- database/seeders/VerificationCodeTableSeeder.php
+++ database/seeders/VerificationCodeTableSeeder.php
@@ -21,18 +21,10 @@ class VerificationCodeTableSeeder extends Seeder
             0 =>
             array (
                 'id' => 1,
-                'user_id' => 3,
-                'code' => 'matrix2012',
-                'created_at' => '2023-05-12 21:07:43',
-                'updated_at' => '2023-05-12 21:07:43'
-            ),
-            1 =>
-            array (
-                'id' => 2,
                 'user_id' => 2,
-                'code' => '901286970',
-                'created_at' => '2023-05-12 21:07:43',
-                'updated_at' => '2023-05-12 21:07:43'
+                'code' => '1005107863',
+                'created_at' => '2024-01-12 21:07:43',
+                'updated_at' => '2024-01-12 21:07:43'
             ),
         ));
     }
