199d608ee191e658506071a95b385ce296c5bf7e
EliasVega <discom.is@gmail.com>
2024-05-22 10:21:39 -0500

    fixed elizabeth

--- database/seeders/PayPaymentSeeder.php
+++ database/seeders/PayPaymentSeeder.php
@@ -4,17 +4,14 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
-class EmployeesTableSeeder extends Seeder
+class PayPaymentSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-            //
+        //
     }
 }
