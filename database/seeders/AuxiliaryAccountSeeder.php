75d380a5f30216c32e9ab2ea7ee10d00a42ef6a5
EliasVega <discom.is@gmail.com>
2024-05-23 08:32:44 -0500

    seeder elizabeth

--- database/seeders/AuxiliaryAccountSeeder.php
+++ database/seeders/AuxiliaryAccountSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class AuxiliaryAccountSeeder extends Seeder
 {
@@ -13,25 +12,6 @@ class AuxiliaryAccountSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('auxiliary_accounts')->delete();
-
-        DB::table('auxiliary_accounts')->insert(array (
-            0 =>
-            array(
-                'id' => 1,
-                'subaccount_id' => 1,
-                'code' => 11050505,
-                'name' => 'AUXILIAR ACCOUNT 1',
-                'total_amount' => 0
-            ),
-            1 =>
-            array(
-                'id' => 2,
-                'subaccount_id' => 2,
-                'code' => 11051010,
-                'name' => 'AUXILIAR ACCOUNT 1',
-                'total_amount' => 0
-            ),
-        ));
+        //
     }
 }
--- database/seeders/SoftwareSeeder.php
+++ database/seeders/SoftwareSeeder.php
@@ -20,16 +20,16 @@ class SoftwareSeeder extends Seeder
             array (
                 'id' => 1,
                 'company_id' => 1,
-                'identifier' => null,
-                'pin' => null,
-                'test_set' => null,
+                'identifier' => '9fab368d-6132-4941-a6ee-73cb681e66f0',
+                'pin' => '98345',
+                'test_set' => '1ced058a-d57b-45f1-8294-beccfc422c3d',
                 'identifier_payroll' => null,
                 'pin_payroll' => null,
                 'payroll_test_set' => null,
                 'identifier_equidoc' => null,
                 'pin_equidoc' => null,
-                'created_at' => '2024-01-01 00:00:00',
-                'updated_at' => '2024-01-01 00:00:00'
+                'created_at' => '2024-05-17 00:00:00',
+                'updated_at' => '2024-01-17 00:00:00'
             ),
         ));
     }
