99ed476a922422f6b2a1cce49d217e7890f212c9
EliasVega <discom.is@gmail.com>
2024-07-01 19:08:08 -0500

    crete request

--- app/Http/Requests/UpdatepayRequest.php
+++ app/Http/Requests/UpdatepayRequest.php
@@ -8,10 +8,8 @@ class UpdatePayRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
-     *
-     * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -19,9 +17,9 @@ class UpdatePayRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, mixed>
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'pay' => 'required',
