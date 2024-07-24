54be9517a8da945afd0ef0c451c1513f7ebb8d97
EliasVega <discom.is@gmail.com>
2024-07-24 11:33:11 -0500

    fixed seders yeison

--- database/seeders/VerificationCodeTableSeeder.php
+++ database/seeders/VerificationCodeTableSeeder.php
@@ -30,7 +30,7 @@ class VerificationCodeTableSeeder extends Seeder
             array (
                 'id' => 2,
                 'user_id' => 2,
-                'code' => '19250556',
+                'code' => '1099368711',
                 'created_at' => '2023-05-12 21:07:43',
                 'updated_at' => '2023-05-12 21:07:43'
             ),
