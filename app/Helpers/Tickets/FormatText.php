2740b4f0b37435a7ac9c9c08942052488deeecaa
EliasVega <discom.is@gmail.com>
2024-07-30 17:46:10 -0500

    fixed formatText

--- app/Helpers/Tickets/FormatText.php
+++ app/Helpers/Tickets/FormatText.php
@@ -7,5 +7,6 @@ use Illuminate\Support\Facades\Http;
 if (!function_exists('formatText')) {
     function formatText($text)
     {
+        return mb_convert_encoding($text, 'ISO-8859-1', 'UTF-8');
     }
 }
