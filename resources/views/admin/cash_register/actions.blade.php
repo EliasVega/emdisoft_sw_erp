47b458e4322ddd2c6a8c6c37e468fc60d1988faa
EliasVega <discom.is@gmail.com>
2024-08-04 07:23:38 -0500

    fixed

--- resources/views/admin/cash_register/actions.blade.php
+++ resources/views/admin/cash_register/actions.blade.php
@@ -5,7 +5,7 @@ data-placement="top" title="Cerrar Caja" ><i class="fas fa-user-lock"></i></a>
 <a href="{{ route('cashRegister.show', $id) }}" class="btn btn-sm btn-success" data-toggle="tooltip"
 data-placement="top" title="ver caja"><i class="fas fa-eye"></i></a>
 <a href="{{ route('posCashRegister', $id) }}" class="btn btn-sm btn-primary" target="_blank" data-toggle="tooltip"
-    data-placement="top" title="PosFpdf"><i class="fas fa-newspaper"></i></a>
+    data-placement="top" title="ticket caja"><i class="fas fa-newspaper"></i></a>
 <a href="{{ route('cashRegisterClose', $id) }}" class="btn btn-sm btn-verde" data-toggle="tooltip" data-placement="top" title="Cierre de caja">
 <i class="fas fa-newspaper"></i></a>
 
