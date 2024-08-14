fb5b4cf492bf4b8dcc7c46e0c63d42714f7dd4e4
EliasVega <discom.is@gmail.com>
2024-08-14 16:40:07 -0500

    fixed request

--- app/Http/Requests/UpdatePayPaymentRequest.php
+++ app/Http/Requests/UpdatePayPaymentRequest.php
@@ -11,7 +11,7 @@ class UpdatePayPaymentRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,12 @@ class UpdatePayPaymentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'pay' => 'required',
+            'balance' => 'numeric',
+            'type' => 'in:purchase,invoice,expense,remission,advance,work_labor,payroll',
+            'user_id' => 'integer',
+            'branch_id' => 'integer'
+
         ];
     }
 }
