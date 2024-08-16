202dcb3778d53c68ba573e75f38c6d4f423bf80f
EliasVega <discom.is@gmail.com>
2024-08-16 17:14:32 -0500

    fixed

--- resources/views/admin/remission/script_edit.blade.php
+++ resources/views/admin/remission/script_edit.blade.php
@@ -18,10 +18,6 @@
             price = value['price'];
             tax_rate = value['tax_rate'];
             pwx = $("#pwx").val();
-            if (pwx == 'on') {
-                taxRate = parseFloat(tax_rate) + 100;
-                price = (parseFloat(price) / parseFloat(taxRate)) * 100;
-            }
             if (product_id != "" && quantity != "" && quantity > 0 && price != "" && price > 0) {
                 subtotal[cont] = parseFloat(quantity) * parseFloat(price);
                 total = total + subtotal[cont];
