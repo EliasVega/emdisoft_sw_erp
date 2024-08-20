615ce423ffaab96792e4109a25ce6a41a74abe43
EliasVega <discom.is@gmail.com>
2024-08-19 21:23:38 -0500

    fixed

--- resources/views/admin/invoice/form_invoice.blade.php
+++ resources/views/admin/invoice/form_invoice.blade.php
@@ -114,7 +114,7 @@
             </div>
         </div>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-8">
         <div class="row">
             <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12" id="noteDocument">
                 <div class="form-group">
