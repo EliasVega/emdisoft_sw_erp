4fc448342e8d40b4a7e3eebd97ddcfa2e82a0a20
EliasVega <discom.is@gmail.com>
2024-08-28 12:06:20 -0500

    fixed

---
+++ resources/views/admin/generalview/form_tablePay.blade.php
@@ -0,0 +1,32 @@
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="addTablePay">
+        <div class="table-responsive">
+            <table id="payments" class="table table-striped table-bordered table-condensed table-hover">
+                <thead class="bg-info">
+                    <tr>
+                        <th>Eliminar</th>
+                        <th>Cta</th>
+                        <th>Medio</th>
+                        <th>T. card</th>
+                        <th>Entidad</th>
+                        <th>Transaccion</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th colspan="6">
+                            <p align="right">TOTAL:</p>
+                        </th>
+                        <th class="thfoot">
+                            <p align="right"><span id="totalpay_html">$ 0.00</span>
+                                <input type="hidden" name="totalpay" id="totalpay">
+                            </p>
+                        </th>
+                    </tr>
+                </tfoot>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+    </div>
